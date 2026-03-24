1181cba3e6a16a1f7f98329979059c8a47928f47
Manuel Latorre <mlatorre@patagon.ai>
2026-03-24 09:53:18 -0300

    feat: add SWR library for data fetching and caching, and update leads retrieval order to descending

--- app/dashboard/actions.ts
+++ app/dashboard/actions.ts
@@ -9,7 +9,7 @@ export async function getLeads() {
     const { data, error } = await supabase
       .from("leads")
       .select(`*, objectives:leads_objectives(*)`)
-      .order("created_at", { ascending: true });
+      .order("created_at", { ascending: false });
 
     if (error) throw new Error(error.message);
     return data;
