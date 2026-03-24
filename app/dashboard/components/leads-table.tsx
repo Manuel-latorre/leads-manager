844ee126d776aed8ff50ed7b0f516b8941cf86d4
Manuel Latorre <mlatorre@patagon.ai>
2026-03-24 13:43:33 -0300

    feat: create LeadsClient component for fetching and displaying lead data using SWR

---
+++ app/dashboard/components/leads-table.tsx
@@ -0,0 +1,28 @@
+"use client"
+
+import useSWR from "swr";
+import { LeadsTable } from "@/components/data-table";
+import { Lead } from "@/app/form/types";
+import { getLeads } from "../actions";
+// Importamos la interface
+
+interface LeadsClientProps {
+  initialData: Lead[]; 
+}
+
+export function LeadsClient({ initialData }: LeadsClientProps) {
+  // SWR ahora sabe exactamente qué forma tiene 'data'
+  const { data } = useSWR<Lead[]>('leads_list', getLeads, { 
+    fallbackData: initialData,
+    revalidateOnMount: false 
+  });
+
+  return (
+    <div className="flex flex-1 flex-col">
+      {/* Si data es undefined (muy raro con fallbackData), 
+         pasamos array vacío que ahora coincide con Lead[] 
+      */}
+      <LeadsTable data={data ?? []} />
+    </div>
+  );
+}
