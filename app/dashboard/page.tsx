7181f792604845fc39f27bbc03b575cdeeed78c7
Manuel Latorre <mlatorre@patagon.ai>
2026-04-02 19:48:20 -0300

    feat: implement user authentication and redirect based on allowed emails in dashboard page

--- app/dashboard/page.tsx
+++ app/dashboard/page.tsx
@@ -3,12 +3,27 @@ import { SiteHeader } from "@/components/site-header";
 import { SidebarInset, SidebarProvider } from "@/components/ui/sidebar";
 import { LeadsClient } from "./components/leads-table";
 import { getLeads } from "./actions";
+import { createClient } from "@/lib/supabase/server";
+import { redirect } from "next/navigation";
 
 export default async function Page() {
 
   // AQUÍ es donde ocurre la magia:
   // Llamamos a la base de datos (Supabase) directamente en el servidor.
   const initialData = await getLeads();
+
+  const supabase = await createClient();
+  const {
+    data: { user },
+  } = await supabase.auth.getUser();
+
+  const allowedEmails =
+  process.env.ALLOWED_EMAILS?.toLowerCase().split(",").map((e) => e.trim()) ?? [];
+
+if (!user?.email || !allowedEmails.includes(user.email.toLowerCase())) {
+  redirect("/form");
+}
+
   return (
     <SidebarProvider
       style={
